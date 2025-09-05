e1c04b92d5f49ebe0c7aa843584569339fe0b312
chenyj03 <chenyj03@mingyuanyun.com>
2025-09-05 16:32:45 +0800

    feat: 修改配置

--- lua/plugin-config/avante.lua
+++ lua/plugin-config/avante.lua
@@ -1,11 +1,14 @@
 require('avante_lib').load()
 require('avante').setup({
-	provider = 'gemini', -- 设置为 Gemini
-	gemini = {
-		endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-		model = "gemini-1.5-flash-latest",
-		timeout = 30000, -- Timeout in milliseconds
-		temperature = 0,
-		max_tokens = 4096,
-	},
+	opts = {
+		provider = {
+			gemini = {
+				endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+				model = "gemini-1.5-flash-latest",
+				timeout = 30000, -- Timeout in milliseconds
+				temperature = 0,
+				max_tokens = 4096,
+			},
+		}, -- 设置为 Gemini
+	}
 })
