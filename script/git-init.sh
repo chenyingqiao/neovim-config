1a8e692a01d379c3f46ab7ed4ebef1d26d0561d4
chenyj03 <chenyj03@mingyuanyun.com>
2026-02-12 14:23:22 +0800

    feat: 设置使用ssh 代替http

--- script/git-init.sh
+++ script/git-init.sh
@@ -2,3 +2,4 @@ git config user.name "chenyj03"
 git config user.email "chenyj03@mingyuanyun.com"
 git config core.filemode false
 git config --global credential.helper cache
+git config --global url."git@git.myscrm.cn:".insteadOf "https://git.myscrm.cn/"
